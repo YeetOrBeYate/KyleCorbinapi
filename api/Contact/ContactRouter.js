454461ba4bec74d3368a1c5f6d6a68aa3a2aa5fd
Kyle Corbin <kylecorbin26@gmail.com>
2020-05-25 20:20:50 -0400

    Will complete the database adjustments and figure out how to send myself an email from the backend

--- api/Contact/ContactRouter.js
+++ api/Contact/ContactRouter.js
@@ -6,7 +6,13 @@ router.post('/', (req,res)=>{
 
     const body= req.body 
 
-    res.status(200).json(body)
+
+    if(body.name !=="fail"){
+        res.status(200).json(body)
+    }else{
+        res.status(401).json(body)
+    }
+
 
 })
 
