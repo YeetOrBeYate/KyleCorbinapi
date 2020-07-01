6ec7a17b700e6d984430010c0e7131cb0a36364f
Kyle Corbin <kylecorbin26@gmail.com>
2020-07-01 18:05:16 -0400

    added other projects pictures

--- data/seeds/zpictures.js
+++ data/seeds/zpictures.js
@@ -2,8 +2,16 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entrie
     return knex('project_pics').insert([
+      {picture:'https://i.imgur.com/surfHQp.png', project_id:1},
+      {picture:'https://i.imgur.com/vbkrFaD.png',project_id:1},
       {picture:'https://i.imgur.com/1PLRstq.png', project_id:2},
-      {picture:'https://i.imgur.com/aE2WpIL.png', project_id:2},
-      {picture:'https://i.imgur.com/aDEyT31.png', project_id:2}
+      {picture:'https://i.imgur.com/aDEyT31.png', project_id:2},
+      {picture:'https://i.imgur.com/csYEvcf.png', project_id:2},
+      {picture:'https://i.imgur.com/5feLXTS.png', project_id:3},
+      {picture:'https://i.imgur.com/jPxUsV3.png', project_id:3},
+      {picture:'https://i.imgur.com/XFDGqoi.png', project_id:3},
+      {picture:'https://i.imgur.com/tTgWM1Y.png', project_id:4},
+      {picture:'https://i.imgur.com/dl7sAjW.png', project_id:4},
+      {picture:'https://i.imgur.com/s71GbJl.png', project_id:4}
     ]);
 };
